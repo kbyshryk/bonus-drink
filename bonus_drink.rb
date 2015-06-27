a6882f305bc79972719e5d1485553f8787f5a64f
hiro <hiroyuki621@gmail.com>
2015-06-27 22:29:59 +0900

    課題への回答

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+
+    # もしボトルが3本いかならそれが飲める全ての飲み物
+    return amount if amount < 3
+
+    # 飲める本数は、 買ったボトルのうち、お店に戻す3の倍数のボトルのほかに、再帰で、お店に戻さない分＋お店から戻ってきたボーナス
+    (amount - (amount % 3)) + total_count_for((amount % 3) + (amount / 3))
+
   end
 end
